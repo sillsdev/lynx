6701b47ce8d852b1a57b1fdae14843b0e43f973d
Damien Daspit <damien_daspit@sil.org>
2024-11-12 17:46:16 -0500

    Use vitest workspaces to run all tests

--- packages/examples/src/verse-order-diagnostic-provider.ts
+++ packages/examples/src/verse-order-diagnostic-provider.ts
@@ -50,7 +50,8 @@ export class VerseOrderDiagnosticProvider implements DiagnosticProvider {
   init(): Promise<void> {
     this.localizer.addNamespace(
       'verseOrder',
-      (language: string) => import(`./locales/${language}/verse-order.json`, { with: { type: 'json' } }),
+      (language: string) =>
+        import(/* @vite-ignore */ `./locales/${language}/verse-order.json`, { with: { type: 'json' } }),
     );
     return Promise.resolve();
   }
