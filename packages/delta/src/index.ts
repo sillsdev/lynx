2b54e07a99af75e79bf7ada1770ee79ccdd43edf
Damien Daspit <damien_daspit@sil.org>
2025-01-23 15:11:32 -0500

    Export ScriptureDeltaDocument and related classes

--- packages/delta/src/index.ts
+++ packages/delta/src/index.ts
@@ -1,3 +1,6 @@
 export { DeltaDocument } from './delta-document';
 export { DeltaDocumentFactory } from './delta-document-factory';
 export { DeltaEditFactory } from './delta-edit-factory';
+export { ScriptureDeltaDocument } from './scripture-delta-document';
+export { ScriptureDeltaDocumentFactory } from './scripture-delta-document-factory';
+export { ScriptureDeltaEditFactory } from './scripture-delta-edit-factory';
