4c34b9d52eb62e5ae6cc3c38960d92a4f1bb6b14
Damien Daspit <damien_daspit@sil.org>
2025-01-27 18:39:51 -0500

    Add reset method to DocumentManager
    - make content an optional parameter in DocumentManager.fireOpened

--- packages/delta/src/delta-document-factory.ts
+++ packages/delta/src/delta-document-factory.ts
@@ -1,18 +1,19 @@
-import { DocumentFactory } from '@sillsdev/lynx';
+import { DocumentChanges, DocumentData, DocumentFactory } from '@sillsdev/lynx';
 import Delta, { Op } from 'quill-delta';
 
 import { DeltaDocument } from './delta-document';
 
 export class DeltaDocumentFactory implements DocumentFactory<DeltaDocument, Op, Delta | string> {
-  create(uri: string, format: string, version: number, content: Delta | string): DeltaDocument {
+  create(uri: string, data: DocumentData<Delta | string>): DeltaDocument {
+    let content = data.content;
     if (typeof content === 'string') {
       content = new Delta(JSON.parse(content));
     }
-    return new DeltaDocument(uri, format, version, content);
+    return new DeltaDocument(uri, data.format, data.version, content);
   }
 
-  update(document: DeltaDocument, changes: Op[], version: number): DeltaDocument {
-    document.update(changes, version);
+  update(document: DeltaDocument, changes: DocumentChanges<Op>): DeltaDocument {
+    document.update(changes.contentChanges, changes.version);
     return document;
   }
 }
